37e73aad114986aaca70259edfc15c9eb6aef03b
Ritika <ahirwarritika27@gmail.com>
2022-09-02 20:36:41 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","Graphics Designer","Content Writer","Competitive Coder"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
